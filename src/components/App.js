7f90cbdeaa3cf3b52c001ebd1dee0ecfb3520b64
Abhishek Mishra <iabm3568@gmail.com>
2026-01-07 17:11:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      <ol key="relativeList">
+        <li key="relativeListItem1">Father</li>
+        <li key="relativeListItem2">Mother</li>
+        <li key="relativeListItem3">Uncle</li>
+        <li key="relativeListItem4">Aunt</li>
+      </ol>
+
+    </div>
+  );
+};
+
+export default App;
